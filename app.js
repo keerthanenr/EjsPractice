409213a5c6e8e8bd6fa59a59551f3f1d15d9c308
keerthanenr <keerthanenr@live.co.uk>
2021-03-06 23:30:44 +0000

    Using layout template to have consistent footer, adding work list

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 var items = ["Buy Food", "Cook Food", "Eat Food"];
+var workItems = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"))
@@ -11,28 +12,41 @@ app.get("/", function(req,res){
   var today  = new Date();
   var currentDay = today.getDay();
   var day = "";
-  //var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  //if(currentDay == 6 || currentDay == 0){
-  //day = days[currentDay];
-  //}else{
-  //  day = "Weekday";
-  //}
-
   var options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
   var day = today.toLocaleDateString("en-US", options);
-  res.render('list', {kindOfDay : day, newListItem: items});
+  res.render('list', {listTitle : day, newListItem: items});
 })
 
 app.post("/", function(req,res){
   var item = req.body.newItem;
-  items.push(item)
-  console.log(item);
-  res.redirect("/");
+  var page = req.body.button;
+  console.log(req.body);
+  if(page =="Work"){
+    workItems.push(item)
+    res.redirect("/work");
+  }else{
+    items.push(item)
+    res.redirect("/");
+  }
 })
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle:"Work List", newListItem: workItems});
+})
+
 app.listen(3000, function(){
   console.log("Server started on port 3000")
+})
+app.post("/work", function(req,res){
+  var workItems = req.body.newItem;
+  workItems.push(item)
+  console.log(req);
+  res.redirect("/work");
+})
+app.get("/about", function(req,res){
+  res.render("about");
 })
