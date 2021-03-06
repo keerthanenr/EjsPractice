4693e4552c76b2d22e05a1621de7510ed9765290
keerthanenr <keerthanenr@live.co.uk>
2021-03-06 22:20:13 +0000

    Updated commit

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.get("/", function(req,res){
   //}else{
   //  day = "Weekday";
   //}
-<<<<<<< HEAD
 
   var options = {
     weekday: "long",
@@ -33,21 +32,6 @@ app.post("/", function(req,res){
   items.push(item)
   console.log(item);
   res.redirect("/");
-=======
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-  var day = today.toLocaleDateString("en-US", options);
-  res.render('list', {kindOfDay : day});
-})
-
-app.post("/", function(req,res){
-  var item = req.body.newItem;
-  console.log(item)
->>>>>>> b5fe894a9bd42cd9238cf2ce31d3b5d373032654
 })
 app.listen(3000, function(){
   console.log("Server started on port 3000")
